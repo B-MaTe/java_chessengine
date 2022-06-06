a80ddbc9c9fc1d0dfafbecc45d5c0a87f36fd0a2
B-MaTe <balazs.mate2002@gmail.com>
2022-06-06 12:25:55 +0200

    moving figure on top of the other figures

---
+++ src/main/java/com/example/chess/Actions.java
@@ -0,0 +1,122 @@
+package com.example.chess;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.InputEvent;
+import java.util.Arrays;
+import java.util.Map;
+
+
+public class Actions extends MouseAdapter {
+    Figures figures;
+    Board board;
+    Settings settings;
+    private byte row, col, oldRow, oldCol;
+    String currPiece;
+    Actions(Board board, Figures figures, Settings settings) {
+        super();
+        this.board = board;
+        this.figures = figures;
+        this.settings = settings;
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getModifiersEx() == InputEvent.BUTTON1_DOWN_MASK) {
+
+            setRow((byte) ((e.getX() - settings.getOffsetX()) / settings.getCellSize()));
+            setCol((byte) ((e.getY() - settings.getOffsetY()) / settings.getCellSize()));
+            for (Map.Entry<String, Byte[]> entry : settings.getFigurePositions().entrySet()) {
+                if (Arrays.equals(entry.getValue(), new Byte[]{getCol(), getRow()})) {
+                    setOldCol(getCol());
+                    setOldRow(getRow());
+                    currPiece = entry.getKey();
+                    //System.out.println(entry.getKey());
+                }
+            }
+                } else if (e.getModifiersEx() == InputEvent.BUTTON2_DOWN_MASK) {
+            //System.out.println("m");
+        } else if (e.getModifiersEx() == InputEvent.BUTTON3_DOWN_MASK) {
+            //System.out.println("r");
+        }
+
+    }
+    public void mouseReleased(MouseEvent e) {
+        currPiece = null;
+        board.setCurrFigure(null);
+
+    }
+
+    public void mouseDragged(MouseEvent e) {
+            if (e.getModifiersEx() == InputEvent.BUTTON1_DOWN_MASK) {
+                if (currPiece != null) {
+                    setRow((byte) ((e.getX() - settings.getOffsetX()) / settings.getCellSize()));
+                    setCol((byte) ((e.getY() - settings.getOffsetY()) / settings.getCellSize()));
+                    if (getRow() < 0 || getRow() > 7 || getCol() < 0 || getCol() > 7) {
+                        setRow(getOldRow());
+                        setCol(getOldCol());
+                    } else {
+                        moveFigure(currPiece);
+                    }
+
+                }
+
+            } else if (e.getModifiersEx() == InputEvent.BUTTON2_DOWN_MASK) {
+
+        } else if (e.getModifiersEx() == InputEvent.BUTTON3_DOWN_MASK) {
+
+        }
+    }
+
+    public void moveFigure(String figure) {
+        //System.out.println(getCol() + "           "  + getRow());
+        board.setCurrFigure(currPiece);
+        settings.moveFigure(figure, new Byte[]{getCol(), getRow()});
+        //System.out.println(Arrays.toString(settings.getFigurePositions().get(figure)));
+    }
+
+    public byte getOldRow() {
+        return oldRow;
+    }
+
+    public void setOldRow(byte oldRow) {
+        this.oldRow = oldRow;
+    }
+
+    public byte getOldCol() {
+        return oldCol;
+    }
+
+    public void setOldCol(byte oldCol) {
+        this.oldCol = oldCol;
+    }
+
+    public Byte getRow() {
+        return row;
+    }
+
+    public void setRow(byte row) {
+        this.row = row;
+    }
+
+    public Byte getCol() {
+        return col;
+    }
+
+    public void setCol(byte col) {
+        this.col = col;
+    }
+
+    public Figures getFigures() {
+        return figures;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Settings getSettings() {
+        return settings;
+    }
+}
